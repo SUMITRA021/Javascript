3d62b160857de8dcd1c043f9d8956a69415b13e2
SUMITRA021 <dawnsumitra021@gmail.com>
2023-07-14 20:21:38 +0530

    Day2 of Javascript

--- Project1/script.js
+++ Project1/script.js
@@ -1,5 +1,23 @@
 let js='amazing';
-            if(js ==='amazing') alert('js is fun!');
-
 console.log(40 + 8 + 23 - 10);
             
+console.log('Sumitra');
+console.log(23);
+console.log('sm' + 98);
+
+
+let firstName = "subho";
+console.log(firstName);
+console.log(firstName);
+
+
+let _function = 29;
+let $function = 29;
+
+
+let PI = 3.14;
+
+let myFirstJob = 'student';
+let myCurrentJob = 'developer';
+
+console.log(myFirstJob);
