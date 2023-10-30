3bba18e3def9a3a6b5f2cef4f51b9f41f428a4f0
SUMITRA021 <dawnsumitra021@gmail.com>
2023-10-31 00:47:28 +0530

    basic of functions

--- Project 2/script.js
+++ Project 2/script.js
@@ -25,3 +25,34 @@ const apporg = fruitProcessor(6, 8);
 console.log(apporg);
 
 //Function declaration Vs expression
+
+// Arrow function
+
+const age4 = (birthYear) => 2023 - birthYear;
+console.log(age4(1996));
+
+const yearsuntilRetirement = (birthyeah, firstname) => {
+  const age6 = 2023 - birthyeah;
+  const retire = 60 - age6;
+  return `${firstname} will retire in ${retire} years`;
+};
+
+console.log(yearsuntilRetirement(1996, "sumitra"));
+
+//Function calling another function
+//One function to cut fruits and another function taking that cut fruits and making juice
+
+function cutFruit(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, organges) {
+  const applePiece = cutFruit(apples);
+  const orgPiece = cutFruit(organges);
+  //console.log(apples, organges);
+  const juice = `juice with ${applePiece} apples piece and ${orgPiece} orange piece.`;
+
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
